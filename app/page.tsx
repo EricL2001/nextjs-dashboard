85d77b644ed0dc67b443c1e342e3c34a89ca6c2e
Eric Levasseur <eric.levass@gmail.com>
2024-10-27 13:07:38 -0400

    fixed actions and create form

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,9 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme Corp.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Acme Corp.</strong> This project was built using {' '}
+            <a href="https://nextjs.org" className="text-blue-500">
+              Next.js </a> and deployed with Vercel.
           </p>
 
           <Link
